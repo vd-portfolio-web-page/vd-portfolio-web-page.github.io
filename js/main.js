4d91ee955813ef557da59a5c5e63802f2dd31851
vitaldo1992 <vitaliy.duvalko@gmail.com>
2016-11-18 02:00:53 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,97 @@
+  	  	$(".scrollto").click(function() {
+  	  		var elementClick = $(this).attr("href")
+  	  		var destination = $(elementClick).offset().top;
+  	  		jQuery("html:not(:animated),body:not(:animated)").animate({
+  	  			scrollTop: destination
+  	  		}, 300);
+  	  		return false;
+  	  	});
+
+
+  	  	var navigationArray = [
+  	  	"#header_label",
+  	  	"#content-content",
+  	  	"#about-me",
+  	  	"#my-background",
+  	  	'#middle-of-background',
+  	  	"#my-contacts",
+  	  	'#footer-menu'
+  	  	]
+
+  	  	window.onscroll = function() {
+
+  	  		if ($(".content-view-portfolio").visible(true)==false) {
+  	  			$('.arrows')[0].style.display = "block"
+  	  		} else {
+  	  			$('.arrows')[0].style.display = "none"
+  	  		}
+
+  	  		if ($(navigationArray[navigationArray.length-1]).visible(true)==true) {
+  	  			$('#arrow-bottom')[0].style.display = "none"
+  	  		} else {
+  	  			$('#arrow-bottom')[0].style.display = "block"
+  	  		}
+
+  	  		for (var i=0; i<navigationArray.length; i++) {
+  	  			if ($(navigationArray[i]).visible(true)==true) {
+  	  				$('#to-up')[0].href=navigationArray[i-1];
+  	  				$('#to-down')[0].href=navigationArray[i+1];
+  	  				if ($(navigationArray[i]).visible(true)==true && $(navigationArray[i-1]).visible(true)==true) {
+  	  					$('#to-up')[0].href=navigationArray[i-2]
+  	  				}
+  	  			}
+  	  		}
+  	  	}
+
+  	  	// window.onscroll =  function(){
+
+
+  	  	// 	if ( $('#my-contacts').visible(true)==true) {
+  	  	// 		$('#arrow-bottom')[0].style.display = "none"
+  	  	// 	} else if ($('#my-contacts').visible(true)==false) {
+  	  	// 		$('#arrow-bottom')[0].style.display = "block"
+  	  	// 	}
+
+  	  	// 	if ($('#content-content').visible(true) == true) {
+
+  	  	// 		(function() {
+  	  	// 			$('#to-up')[0].href="#footer_label";
+  	  	// 			$('#to-down')[0].href="#about-me";
+  	  	// 		})()
+
+  	  	// 	}
+  	  	// 	if ($('#about-me').visible(true) == true) {
+  	  	// 		(function() {
+  	  	// 			$('#to-up')[0].href="#content-content";
+  	  	// 			$('#to-down')[0].href="#my-background";
+  	  	// 		})()
+
+  	  	// 	}
+  	  	// 	if ($('#my-background').visible(true) == true) {
+
+  	  	// 		(function() {
+  	  	// 			$('#to-up')[0].href="#about-me";
+  	  	// 			$('#to-down')[0].href="#middle-of-background";
+  	  	// 		})()
+
+  	  	// 	}
+  	  	// 	if ($('#middle-of-background').visible(true) == true) {
+  	  	// 		(function(){
+  	  	// 			$('#to-up')[0].href="#my-background";
+  	  	// 			$('#to-down')[0].href="#my-contacts";
+  	  	// 		})()
+
+  	  	// 	}
+  	  	// 	if ($('#my-contacts').visible(true) == true) {
+  	  	// 		(function() {
+  	  	// 			$('#to-up')[0].href='#middle-of-background';
+  	  	// 			$('#to-down')[0].href="#my-contacts";
+  	  	// 		})()
+
+  	  	// 	}
+  	  	// }
+
+
+
+
+
